9921ce6483a24c13c4473159b26829142bd9b9fe
rivanildojr <rivanildo_junior7@hotmail.com>
2024-02-13 00:09:17 -0300

    fix[Quiz]: remove comments

--- src/constants/questions.js
+++ src/constants/questions.js
@@ -62,214 +62,214 @@ export const QUESTIONS = [
     ],
     selected: null
   },
-  // {
-  //   question: 'Na sua opiniao, seus braços e pernas são:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Curtos e engrossados'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Possuem aspecto normal, com musculos visiveis'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Longos e finos '
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Quando criança, voce era:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Gordinho / ganhava peso facil'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Normal'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Magrinho '
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Seu metabolismo é:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Lento'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Normal'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Muito rápido'
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Sobre seu peso, voce:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Ganha peso com facilidade, e tem dificuldade para perder'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Ganha peso ou perde facilmente, mas geralmente se mantem num peso ideal'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Tem dificuldade para ganhar peso'
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Sobre seu apetite, voce sente fome:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Praticamente o dia todo'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Apetite controlado, sinto mais fome nos horarios das refeiçoes'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Nao sinto muita fome'
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'As pessoas falam que seu fisico aparenta:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Inchado, retido, grande, acima do peso'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Normal, porte atlético, nao precisa nem ganhar ou baixar peso'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Magrelo, raquítico, abaixo do peso'
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Que tamanho de camiseta voce usa',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'G , GG, ou EXTRA G'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO ',
-  //       option: 'M ou no máximo G'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Geralmente P, às vezes M'
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Quando voce compra roupas, geralmente elas ficam:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Apertadas, preciso pegar tamanhos grandes para que nao fiquem desconfortáveis'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Normais e vestem bem'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: ' Largas, principalmente nos braços'
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Nesse momento, como voce descreve seu físico:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Estou acima do peso, quero perder gordura e definir '
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Estou num peso ideal, porem quero mais definiçao'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Estou abaixo do peso, quero ganhar volume muscular'
-  //     }
-  //   ],
-  //   selected: null
-  // },
-  // {
-  //   question: 'Sua linha de cintura, naturalmente sempre foi:',
-  //   options: [
-  //     {
-  //       id: 0,
-  //       biotype: 'ENDOMORFO',
-  //       option: 'Larga, grossa e redonda'
-  //     },
-  //     {
-  //       id: 1,
-  //       biotype: 'MESOMORFO',
-  //       option: 'Normal'
-  //     },
-  //     {
-  //       id: 2,
-  //       biotype: 'ECTOMORFO',
-  //       option: 'Pequena, fina'
-  //     }
-  //   ],
-  //   selected: null
-  // }
+  {
+    question: 'Na sua opiniao, seus braços e pernas são:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Curtos e engrossados'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Possuem aspecto normal, com musculos visiveis'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Longos e finos '
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Quando criança, voce era:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Gordinho / ganhava peso facil'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Normal'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Magrinho '
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Seu metabolismo é:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Lento'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Normal'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Muito rápido'
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Sobre seu peso, voce:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Ganha peso com facilidade, e tem dificuldade para perder'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Ganha peso ou perde facilmente, mas geralmente se mantem num peso ideal'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Tem dificuldade para ganhar peso'
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Sobre seu apetite, voce sente fome:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Praticamente o dia todo'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Apetite controlado, sinto mais fome nos horarios das refeiçoes'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Nao sinto muita fome'
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'As pessoas falam que seu fisico aparenta:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Inchado, retido, grande, acima do peso'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Normal, porte atlético, nao precisa nem ganhar ou baixar peso'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Magrelo, raquítico, abaixo do peso'
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Que tamanho de camiseta voce usa',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'G , GG, ou EXTRA G'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO ',
+        option: 'M ou no máximo G'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Geralmente P, às vezes M'
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Quando voce compra roupas, geralmente elas ficam:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Apertadas, preciso pegar tamanhos grandes para que nao fiquem desconfortáveis'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Normais e vestem bem'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: ' Largas, principalmente nos braços'
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Nesse momento, como voce descreve seu físico:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Estou acima do peso, quero perder gordura e definir '
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Estou num peso ideal, porem quero mais definiçao'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Estou abaixo do peso, quero ganhar volume muscular'
+      }
+    ],
+    selected: null
+  },
+  {
+    question: 'Sua linha de cintura, naturalmente sempre foi:',
+    options: [
+      {
+        id: 0,
+        biotype: 'ENDOMORFO',
+        option: 'Larga, grossa e redonda'
+      },
+      {
+        id: 1,
+        biotype: 'MESOMORFO',
+        option: 'Normal'
+      },
+      {
+        id: 2,
+        biotype: 'ECTOMORFO',
+        option: 'Pequena, fina'
+      }
+    ],
+    selected: null
+  }
 ];
